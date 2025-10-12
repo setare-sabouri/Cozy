8513e59a92d25b65da81b883cf24e25dc427e807
setareh <setarehsaboory@gmail.com>
2025-10-12 03:39:35 +0200

    details display

--- src/Interface/Details.jsx
+++ src/Interface/Details.jsx
@@ -4,30 +4,41 @@ import styles from './Interface.module.scss'
 const Details = () => {
     const { WeatherData, CityName } = useStore()
     console.log(WeatherData)
+    const  localTime= new Date((WeatherData?.dt + WeatherData?.timezone)*1000)
 
     return (
         <>
-            <div className={styles.DetailsContainer}>
-                <div className={styles.CityWeather}>
-                    <h1>
-                        {WeatherData && CityName
-                            ? `${WeatherData.weather[0].main} in ${CityName}`
-                            : 'Search a city'}
-                    </h1>
+            {WeatherData &&
+                <div className={styles.DetailsContainer}>
+                    <div className={styles.CityWeather}>
+                        <h1>
+                            {WeatherData && CityName
+                                ? `${WeatherData.weather[0].main} in ${CityName}`
+                                : 'Search a city'}
+                        </h1>
+                    </div>
+                    <div className={styles.weatherDetails}>
+                        <p>
+                            `$ coutry is {WeatherData.sys.country}`
+                        </p>
+                        <p>
+                            {`${Math.round(WeatherData.main.temp - 273.15)} °`}
+                        </p>
+                        <p>
+                            {` Highest ${Math.round(WeatherData.main.temp_max - 273.15)} ° Lowest ${Math.round(WeatherData.main.temp_min - 273.15)} °`}
+                        </p>
+                        <p>
+                            {`Feels like ${Math.round(WeatherData.main.feels_like - 273.15)} °`}
+                        </p>
+                        <p>
+                            {`Humidity ${WeatherData.main.humidity} %`}
+                        </p>
+                        <p>
+                            {`Time ${localTime.toLocaleString("en-SE", { timeZone: "UTC" })} `}
+                        </p>
+                    </div>
                 </div>
-                <div className={styles.weatherDetails}>
-                    <p>
-                        {`${Math.round(WeatherData.main.temp - 273.15 )} °`}
-                    </p>
-                    <p>
-                        `$ coutry is {WeatherData?.sys.country}`
-                    </p>
-                    <p>
-                        CCCC
-                    </p>
-
-                </div>
-            </div>
+            }
         </>
     )
 }
